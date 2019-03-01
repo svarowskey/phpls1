0ff04e9e8771a3f1f26ec31864218609e1698020
svarowskey <38376484+svarowskey@users.noreply.github.com>
2019-03-01 18:01:21 +0600

    update index.php

--- index.php
+++ index.php
@@ -5,4 +5,3 @@ var_dump($a == $b);         // Почему true?
 var_dump((int)'012345');     // Почему 12345?
 var_dump((float)123.0 === (int)123.0); // Почему false?
 var_dump((int)0 === (int)'hello, world'); // Почему true?
-
