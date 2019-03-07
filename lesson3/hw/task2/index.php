bcfb38a093a413fd7bda5d15ceadab72f5de4a79
svarowskey <vladimir.svarowski@gmail.com>
2019-03-08 01:40:05 +0600

    Lesson 3 homework task 2

---
+++ lesson3/hw/task2/index.php
@@ -0,0 +1,15 @@
+<?php
+$i = 0;
+do {
+    if ($i === 0) {
+        echo $i . " - это ноль <br>";
+        $i++;
+        continue;
+    } else if ($i % 2 === 0){
+        echo $i . " - это четное число <br>";
+        $i++;
+        continue;
+    }
+    echo $i . " - это нечетное число <br>";
+    $i++;
+} while ($i <= 10);
